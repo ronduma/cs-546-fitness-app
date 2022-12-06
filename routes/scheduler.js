83ecfc8a065469927764d4579910119908420649
Erik Anhorn <erikjanhorn@gmail.com>
2022-12-06 10:12:57 -0500

    Basic work on scheduler

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -4,11 +4,20 @@ const data = require('../data');
 const path = require('path');
 
 router.get('/', async (req, res) => {
-    res.status(200).render('scheduler', {
-        title : "Scheduler \• Jimbro",
-        message : "this is the scheduler page",
-        session : req.session.user
-    });
+    if(req.session.user){
+        res.status(200).render('scheduler', {
+            title : "Scheduler \• Jimbro",
+            message : "this is the scheduler page",
+            session : req.session.user
+        });
+    }
+    else{
+        res.status(200).render('scheduler', {
+            title : "Scheduler \• Jimbro",
+            message : "this is the scheduler page",
+            session : req.session.user
+        });
+    }
 });
 
 module.exports = router;
