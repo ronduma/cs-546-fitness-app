e6e6e3091daf17c545aeec1dda83d036b55f574e
Ron Dumalagan <rdumalag@stevens.edu>
2022-12-15 20:19:20 -0500

    more frontend stuff

--- data/index.js
+++ data/index.js
@@ -2,7 +2,7 @@ const userData=require('./users');
 const postData=require('./posts');
 const workoutData = require('./workouts');
 module.exports = {
-    user: userData,
+    users: userData,
     post: postData,
     workouts: workoutData
     
