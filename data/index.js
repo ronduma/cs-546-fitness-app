6f2e4d38189205a12058c3219ac70a2ae33359d6
Jae <84739325+jaejang02@users.noreply.github.com>
2022-12-15 20:00:44 -0500

    Random commit

--- data/index.js
+++ data/index.js
@@ -1,10 +1,12 @@
 const postsData = require('./posts');
 const usersData = require('./users');
 const workoutData = require('./workouts');
-
+const statsDiagram = require('./statsDiagram');
+const progress = require('./progress');
 module.exports = {
     posts: postsData,
     users: usersData,
-    workouts: workoutData
-    
+    workouts: workoutData,
+    statsDiagram: statsDiagram
+
 };
