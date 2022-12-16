6191b37f10b839efb871fbe59434715736fd6687
Jae <84739325+jaejang02@users.noreply.github.com>
2022-12-16 17:21:54 -0500

    Fixed UserStats

--- routes/statsDiagram.js
+++ routes/statsDiagram.js
@@ -4,16 +4,38 @@ const helpers = require('../helpers');
 const users = require('../data/users');
 const stats = require('../data/statsDiagram');
 
-router.get('/', async (req,res) => {
+// router.get('/', async (req,res) => {
+//     if(req.session.user){
+//         return res.status(200).render('statsDiagram')
+//     }
+//     else{
+//         return res.redirect('./login');
+//     }
+// });
+
+router.get('/', async (req, res) =>{
+
     if(req.session.user){
-        return res.status(200).render('statsDiagram')
+        let option1 = "BodyGroup Graph";
+        let option2 = "Excercise Progress";
+
+        return res.status(200).render('statsScroll',  {
+            title: "Sign Up \• Jimbro",
+            message: "Stats Scroll wheel",
+            session: req.session.user,
+            option1: option1,
+            option2: option2
+        });
     }
     else{
         return res.redirect('./login');
     }
 });
-router.get('/bodygroup', async (req,res) => {
-    if(req.session.user){
+//post
+router.post('/', async (req,res) => {
+    let search = req.body;
+    search = search.scroll;
+    if(search === "BodyGroup Ratio"){
         let user = await users.getUserByUsername(req.session.user);
         let userId = user._id;
         let upper = await stats.upperData(userId);
@@ -25,7 +47,7 @@ router.get('/bodygroup', async (req,res) => {
         let lower_percentage = (lower.length / (upper.length + core.length + lower.length)) * 100;
         
 
-        return res.status(200).render('statsDiagram', {
+        return res.status(200).render('BodyGroup', {
             title: "Sign Up \• Jimbro",
             message: req.session.user + " Diagram",
             session: req.session.user,
@@ -35,9 +57,40 @@ router.get('/bodygroup', async (req,res) => {
 
         });
     }
+    else if(search === "Workout Progress"){
+        let current = await users.getUserByUsername(req.session.user);
+        let lst = await stats.getAllExcercise(current._id);
+        let result = [];
+        for(let i = 0; i < lst.length; i++){
+            result.push(lst[i].name);
+        }
+        return res.status(200).render('WorkoutProgress', {
+            title: "Sign Up \• Jimbro",
+            message: "Pick progress",
+            session: req.session.user,
+            list: result
+        });
+    }
     else{
         return res.redirect('./login');
     }
+});
+
+router.post('/workoutProgress', async (req,res) => {
+    let search = req.body;
+    search = search.scroll;
+    console.log(search);
+    let current = await users.getUserByUsername(req.session.user);
+    let current_data = await stats.getWeights(current._id, search);
+    let current_days = await stats.getDays(current._id, search);
+    console.log(current_data);
+    return res.status(200).render('workoutGraph', {
+        title: "Sign Up \• Jimbro",
+        message: req.session.user + " " + search + " weight progress",
+        session: req.session.user,
+        legend: current_days,
+        data: current_data
+    });
 });
 
 module.exports = router;
