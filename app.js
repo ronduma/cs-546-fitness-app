d4aeb961a7607f948fb72411e778ae2595db5e1e
Jae <84739325+jaejang02@users.noreply.github.com>
2022-12-18 18:19:41 -0500

    Post Delete works with bugs fixed

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ app.use(express.urlencoded({extended: true}));
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+const rewriteUnsupportedBrowserMethods = (req, res, next) => {
+  // If the user posts to the server with a property called _method, rewrite the request's method
+  // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
+  // rewritten in this middleware to a PUT route
+  if (req.body && req.body._method) {
+    req.method = req.body._method;
+    delete req.body._method;
+  }
+
+  // let the next middleware run:
+  next();
+};
+
+app.use(rewriteUnsupportedBrowserMethods);
+
 app.use(
   session({
     name: 'AuthCookie',
